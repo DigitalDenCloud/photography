36b084dd1a8ce012e16e015f514ccc5f70910504
Deniz Yilmaz <denizyilmaz@hotmail.co.uk>
2025-11-22 22:22:42 +0000

    Update exif patch

--- assets/js/exif-patch.js
+++ assets/js/exif-patch.js
@@ -2,3 +2,17 @@
 if (typeof EXIF !== 'undefined' && EXIF.Tags) {
     EXIF.Tags[0x0095] = "LensModel";
 }
+
+// Handle unmapped Sony lens tags
+(function() {
+    var originalGetTag = EXIF.getTag;
+    EXIF.getTag = function(img, tag) {
+        var result = originalGetTag.call(this, img, tag);
+        if (result === undefined && img.exifdata && img.exifdata['undefined']) {
+            if (tag === 'LensModel') {
+                return img.exifdata['undefined'];
+            }
+        }
+        return result;
+    };
+})();
